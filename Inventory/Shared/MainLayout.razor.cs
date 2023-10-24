907a94d6773136b54d66f96e3356985bb8936ec6
Vitalii Khrystov <vitalik_khristov@ukr.net>
2023-10-24 11:06:14 +0300

    Add Logout

---
+++ Inventory/Shared/MainLayout.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components;
+using Inventory.Authentication;
+
+namespace Inventory.Shared
+{
+    public partial class MainLayout
+    {
+        [Inject] private AuthenticationStateProvider authStateProvider { get; set; }
+        [Inject] private NavigationManager navManager { get; set; }
+        public async Task Logout()
+        {
+            var customAuthStateProvider = (CustomAuthenticationProvider)authStateProvider;
+            await customAuthStateProvider.UpdateAuthenticationState(null);
+            navManager.NavigateTo("/", true);
+        }
+    }
+}
